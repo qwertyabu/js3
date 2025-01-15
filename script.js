bbf34880434387f09bb309f1c10ba46fc28f7bb3
qwertyabu <monkey.famil4@gmail.com>
2025-01-15 20:29:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let one = ('siga hozir random sonlar chiqadi :) ')
+alert(one)
+
+let two = Math.random() * 100;
+two = Math.ceil(two)
+alert(two)
+
+let seven = ("biz random soni 2ga kopaytirmoqchimiz va bolmohchimiz")
+alert(seven)
+
+let four = (`${2*two}`)
+console.log(four)
+
+let five = (`${four/2}`)
+console.log(four)
+
+let six = (`${five/3}`)
+console.log(five)
+
+
+
+  let three = (`biz ogan son ${two} edi
+    biz uni 2 kopaytirip ${four}
+    va shu soni olib 2 bolamiza ${five}
+    va shu soni 3 ga bolgandagi qoldiq ${six} `)
+  alert(three)
+
+
+
